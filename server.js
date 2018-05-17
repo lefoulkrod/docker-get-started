570cc3b585ef962b89a412ddb62f3a2c4c076458
Larry Foulkrod <lefoulkrod@gmail.com>
2018-05-17 11:14:00 -0500

    part 3

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var http = require('http');
 
+var os = require("os");
+var hostname = os.hostname();
+
 http.createServer(function(request, response){
   response.writeHead(200, {'Content-type':'text/plan'});
-  response.write('Hello ' + process.env.NAME);
+  response.write('Hello ' + process.env.NAME + ' ' + hostname);
   response.end( );
 
 }).listen(80);
